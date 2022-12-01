bb40a2e636417a594f2c6d943a93c4c5dfe0cc5e
Christopher Davidson <chdavids@cisco.com>
2022-12-01 09:09:11 +0000

    playing with rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,7 @@
+mod d01;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "../input/d01-example";
+    println!("{}", d01::p1(file_path).to_string());
+    println!("{}", d01::p2(file_path).to_string());
 }

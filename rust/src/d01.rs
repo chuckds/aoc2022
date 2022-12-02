72a4b61b97c90e03069755d1df276f46bb6100dc
Christopher Davidson <chdavids@cisco.com>
2022-12-02 23:33:24 +0000

    rust d1

--- rust/src/d01.rs
+++ rust/src/d01.rs
@@ -1,22 +1,41 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-use std::collections::VecDeque;
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
+use crate::utils;
 
 
 pub fn p1p2(input_file: &str) -> (i32, i32) {
-    let increasing = 0;
-    let depths: VecDeque<u32> = VecDeque::new();
-    let lines = read_lines(input_file);
-    // Consumes the iterator, returns an (Optional) String
-    for cal in lines {
-        let cal = cal.parse::<i32>().unwrap();
+    let mut current_sum = 0;
+    let mut sorted_elves = BinaryHeap::with_capacity(4);
+
+    for line in utils::read_lines(input_file).unwrap() {
+        let cal = line.unwrap();
+        if cal.is_empty() {
+            sorted_elves.push(Reverse(current_sum));
+            if sorted_elves.len() > 3 {
+                sorted_elves.pop();
+            }
+            current_sum = 0;
+        } else{
+            current_sum += cal.parse::<i32>().unwrap();
+        }
     }
-    (34, 12)
+    sorted_elves.push(Reverse(current_sum));
+    if sorted_elves.len() > 3 {
+        sorted_elves.pop();
+    }
+
+    // what is going on here - this doesn't look good.
+    let sorted_elves = sorted_elves.into_sorted_vec();
+    // Unpack the values out of Reverse otherwise sum doesn't work - wat?
+    let sorted_elves: Vec<i32> = sorted_elves.iter().map(|Reverse(val)| *val).collect();
+
+    (*sorted_elves.first().unwrap(), sorted_elves.iter().sum())
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+
+#[test]
+fn d01_test() {
+    assert_eq!((24000, 45000), p1p2("../input/d01-example"));
+    assert_eq!((71924, 210406), p1p2("../input/d01"));
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,43 @@
 mod d01;
+mod utils;
+
 
 fn main() {
-    let file_path = "../input/d01-example";
-    println!("{}", d01::p1p2(file_path).to_string());
+    let (p1, p2) = d01::p1p2("../input/d01");
+    println!("p1: {}, p2: {}", p1, p2);
+}
+
+#[test]
+fn test() {
+    use std::collections::HashMap;
+    use regex::Regex;
+
+    let part_to_function = HashMap::from([
+        ("d01p1p2", d01::p1p2),
+    ]);
+
+    let answer_re = Regex::new(r"^(?P<part>d[^ ]+) (?P<input_file>[^ ]+) (?P<result>.*)").unwrap();
+    let result_re = Regex::new(r"\((?P<p1>[0-9]+), (?P<p2>[0-9]+)\)").unwrap();
+    for line in utils::read_lines("../answers").unwrap() {
+        let line = line.unwrap();
+        if let Some(caps) = answer_re.captures(&line) {
+            let part = &caps["part"];
+            let input_file = &caps["input_file"];
+            let result = &caps["result"];
+            if let Some(test_func) = part_to_function.get(part) {
+                if let Some(retval_caps) = result_re.captures(&result) {
+                    let result = (retval_caps["p1"].parse::<i32>().unwrap(),
+                                  retval_caps["p2"].parse::<i32>().unwrap());
+                    assert_eq!(result,
+                              test_func(&format!("{}{}", "../input/", input_file)));
+                } else {
+                    println!("Failed to parse expected result {}", result);
+                }
+            } else {
+                println!("No function for {}", part);
+            }
+            
+        }
+
+    }
 }

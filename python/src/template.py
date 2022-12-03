f0013d5ba2fd8be55235c6efe73d7217b94f2270
Christopher Davidson <chdavids@cisco.com>
2022-12-03 10:28:32 +0000

    update template

--- python/src/template.py
+++ python/src/template.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 """
-Advent Of Code 2022 Day X
+Advent Of Code 2022 XXX
 """
 
 from __future__ import annotations
@@ -12,7 +12,7 @@ from pathlib import Path
 repo_root = Path(__file__).parent.parent.parent
 
 
-def p1p2(input_file: Path = repo_root / "input" / "d02-example") -> tuple[int, int]:
+def p1p2(input_file: Path = repo_root / "input" / "XXX-example") -> tuple[int, int]:
     p1, p2 = (0, 0)
 
     return (p1, p2)

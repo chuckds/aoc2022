b31d144c33caf5231dd5712a287538d8fdec1716
Christopher Davidson <chdavids@cisco.com>
2022-12-05 14:29:08 +0000

    python d05

---
+++ python/src/d05.py
@@ -0,0 +1,54 @@
+#!/bin/env python3
+"""
+Advent Of Code 2022 Day 5
+"""
+
+from __future__ import annotations
+
+
+from collections import deque
+from typing import NamedTuple
+from pathlib import Path
+
+
+repo_root = Path(__file__).parent.parent.parent
+
+
+class Move(NamedTuple):
+    num_move: int
+    from_stack: int
+    to_stack: int
+
+
+def p1p2(input_file: Path = repo_root / "input" / "d05-example") -> tuple[str, str]:
+    p1, p2 = ("", "")
+
+    stacks: dict[int, deque[str]] = {}
+    moves: list[Move] = []
+    stack_phase = True
+    for line in input_file.read_text().splitlines():
+        if stack_phase:
+            for stack_num, stack_char in enumerate(line[1::4]):
+                if stack_char == "1":
+                    stack_phase = False
+                    break
+                elif stack_char != " ":
+                    stacks.setdefault(stack_num + 1, deque()).appendleft(stack_char)
+        elif line:
+            tokens = line.split()
+            moves.append(Move(*(int(tokens[i]) for i in (1, 3, 5))))
+
+    p1stacks = {stack_num: stack.copy() for stack_num, stack in stacks.items()}
+    for move in moves:
+        for _ in range(move.num_move):
+            p1stacks[move.to_stack].append(p1stacks[move.from_stack].pop())
+        to_move = []
+        for _ in range(move.num_move):
+            to_move.append(stacks[move.from_stack].pop())
+        to_move.reverse()
+        stacks[move.to_stack].extend(to_move)
+
+    for stack_num in range(1, len(stacks) + 1):
+        p1 += p1stacks[stack_num].pop()
+        p2 += stacks[stack_num].pop()
+    return (p1, p2)
---
+++ rust/src/d05.rs
@@ -0,0 +1,33 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
+use crate::utils;
+
+pub fn p1p2(input_file: &str) -> (i32, i32) {
+    let mut current_sum = 0;
+    let mut sorted_elves = BinaryHeap::with_capacity(4);
+
+    for line in utils::read_lines(input_file).unwrap() {
+        let cal = line.unwrap();
+        if cal.is_empty() {
+            sorted_elves.push(Reverse(current_sum));
+            if sorted_elves.len() > 3 {
+                sorted_elves.pop();
+            }
+            current_sum = 0;
+        } else {
+            current_sum += cal.parse::<i32>().unwrap();
+        }
+    }
+    sorted_elves.push(Reverse(current_sum));
+    if sorted_elves.len() > 3 {
+        sorted_elves.pop();
+    }
+
+    // what is going on here - this doesn't look good.
+    let sorted_elves = sorted_elves.into_sorted_vec();
+    // Unpack the values out of Reverse otherwise sum doesn't work - wat?
+    let sorted_elves: Vec<i32> = sorted_elves.iter().map(|Reverse(val)| *val).collect();
+
+    (*sorted_elves.first().unwrap(), sorted_elves.iter().sum())
+}

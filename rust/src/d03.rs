eee58d09ddfa0f04583d768c633b86891227d6d5
Christopher Davidson <chdavids@cisco.com>
2022-12-06 11:33:59 +0000

    rust d3

--- rust/src/d03.rs
+++ rust/src/d03.rs
@@ -1,33 +1,38 @@
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+use std::collections::HashSet;
 
 use crate::utils;
 
-pub fn p1p2(input_file: &str) -> (i32, i32) {
-    let mut current_sum = 0;
-    let mut sorted_elves = BinaryHeap::with_capacity(4);
+fn char_to_prio(a_char: char) -> i32 {
+    let offset = if a_char.is_ascii_lowercase() {96} else {38};
+    return a_char as i32 - offset
+}
 
+pub fn p1p2(input_file: &str) -> (i32, i32) {
+    let mut p1: i32 = 0;
+    let mut p2: i32 = 0;
+
+    let mut group_bags: Vec<HashSet<char>> = Vec::with_capacity(3);
     for line in utils::read_lines(input_file).unwrap() {
-        let cal = line.unwrap();
-        if cal.is_empty() {
-            sorted_elves.push(Reverse(current_sum));
-            if sorted_elves.len() > 3 {
-                sorted_elves.pop();
+        let item_list = line.unwrap();
+
+        let comp1 = &item_list[..(item_list.len() / 2)];
+        let comp2 = &item_list[(item_list.len() / 2)..];
+        let comp1: HashSet<char> = HashSet::from_iter(comp1.chars());
+        let comp2: HashSet<char> = HashSet::from_iter(comp2.chars());
+        let common_char = comp1.intersection(&comp2).next().expect("No items in common");
+        p1 += char_to_prio(*common_char);
+
+        // Part 2
+        group_bags.push(HashSet::from_iter(item_list.chars()));
+        if group_bags.len() == 3 {
+            for a_char in &group_bags[0] {
+                if group_bags[1].contains(&a_char) && group_bags[2].contains(&a_char) {
+                    p2 += char_to_prio(*a_char);
+                    break
+                }
             }
-            current_sum = 0;
-        } else {
-            current_sum += cal.parse::<i32>().unwrap();
+            group_bags.clear();
         }
     }
-    sorted_elves.push(Reverse(current_sum));
-    if sorted_elves.len() > 3 {
-        sorted_elves.pop();
-    }
-
-    // what is going on here - this doesn't look good.
-    let sorted_elves = sorted_elves.into_sorted_vec();
-    // Unpack the values out of Reverse otherwise sum doesn't work - wat?
-    let sorted_elves: Vec<i32> = sorted_elves.iter().map(|Reverse(val)| *val).collect();
-
-    (*sorted_elves.first().unwrap(), sorted_elves.iter().sum())
+    (p1, p2)
 }

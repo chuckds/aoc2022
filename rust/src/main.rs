e328579d61107e4795ae03874403f854ec3faf45
Christopher Davidson <chdavids@cisco.com>
2022-12-06 12:03:36 +0000

    Update rust test strat

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,54 +1,54 @@
 mod d01;
 mod d02;
+mod d03;
 mod utils;
+
+use std::collections::HashMap;
+
+use rstest::rstest;
 
 fn main() {
     println!("{:?}", d01::p1p2("../input/d01-example"));
     println!("{:?}", d02::p1p2("../input/d02-example"));
+    println!("{:?}", d03::p1p2("../input/d03-example"));
+    let _ = test_hash();
 }
 
-type AoCSolver = fn(&str) -> (i32, i32);
-
-#[test]
-fn test() {
-    use regex::Regex;
-    use std::collections::HashMap;
-
-    let part_to_function: HashMap<&str, AoCSolver> = HashMap::from([
-        ("d01p1p2", d01::p1p2 as AoCSolver),
-        ("d02p1p2", d02::p1p2 as AoCSolver),
-    ]);
-
-    let answer_re = Regex::new(r"^(?P<part>d[^ ]+) (?P<input_file>[^ ]+) (?P<result>.*)").unwrap();
-    let int_result_re = Regex::new(r"\((?P<p1>[0-9]+), (?P<p2>[0-9]+)\)").unwrap();
+#[rstest::fixture]
+#[once]
+fn test_hash() -> HashMap<String, (String, String)> {
+    println!("Loading test hash");
+    let mut test_hash = HashMap::new();
+    let answer_re = regex::Regex::new(r"^(?P<part>d[^ ]+) (?P<input_file>[^ ]+) (?P<result>.*)").unwrap();
     //let str_result_re = Regex::new(r"\(\"(?P<p1>.+)\", \"(?P<p2>.+)\"\)").unwrap();
     for line in utils::read_lines("../answers").unwrap() {
         let line = line.unwrap();
         if let Some(caps) = answer_re.captures(&line) {
-            let part = &caps["part"];
-            let input_file = &caps["input_file"];
-            let result = &caps["result"];
-            if let Some(test_func) = part_to_function.get(part) {
-                if let Some(retval_caps) = int_result_re.captures(&result) {
-                    let result = (
-                        retval_caps["p1"].parse::<i32>().unwrap(),
-                        retval_caps["p2"].parse::<i32>().unwrap(),
-                    );
-                    assert_eq!(result, test_func(&format!("{}{}", "../input/", input_file)));
-                //} else if let Some(retval_caps) = str_result_re.captures(&result) {
-                //    let result = (
-                //        &retval_caps["p1"],
-                //        &retval_caps["p2"],
-                //    );
-                //    assert_eq!(result, test_func(&format!("{}{}", "../input/", input_file)));
-                } else {
-                    println!("Failed to parse expected result {}", result);
-                }
-            } else {
-                println!("No function for {}", part);
-            }
+            test_hash.insert(caps["part"].to_string(), (caps["input_file"].to_string(), caps["result"].to_string()));
         } else {
             println!("Failed to parse {}", line);
         }
     }
+    test_hash
+}
+
+type AoCSolver = fn(&str) -> (i32, i32);
+
+fn run_day_test(day_part: &str, test_func: AoCSolver, test_hash: &HashMap<String, (String, String)>) {
+    let (input_file, expected_result) = test_hash.get(day_part).expect("Can't find day in hash");
+    let int_result_re = regex::Regex::new(r"\((?P<p1>[0-9]+), (?P<p2>[0-9]+)\)").unwrap();
+    let retval_caps = int_result_re.captures(&expected_result).expect("Failed to parse result");
+    let expected_result = (
+        retval_caps["p1"].parse::<i32>().unwrap(),
+        retval_caps["p2"].parse::<i32>().unwrap(),
+    );
+    assert_eq!(expected_result, test_func(&format!("{}{}", "../input/", input_file)));
+}
+
+#[rstest]
+#[case("d01p1p2", d01::p1p2 as AoCSolver)]
+#[case("d02p1p2", d02::p1p2 as AoCSolver)]
+#[case("d03p1p2", d03::p1p2 as AoCSolver)]
+fn day_test(test_hash: &HashMap<String, (String, String)>, #[case] day_str: &str, #[case] test_func: AoCSolver) {
+    run_day_test(day_str, test_func, test_hash);
 }

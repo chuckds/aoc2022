460e34ec97adc487582f808d542c43e5d7ec5958
Christopher Davidson <chdavids@cisco.com>
2022-12-07 01:29:23 +0000

    fmt

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -44,7 +44,9 @@ fn json_to_aocresult(json_val: &json::JsonValue) -> AoCResult {
     match json_val {
         json::JsonValue::Short(a_string) => AoCResult::String(a_string.to_string()),
         json::JsonValue::Number(_num) => AoCResult::Number(json_val.as_i32().unwrap()),
-        json::JsonValue::Array(vec) => AoCResult::NumVec(vec.iter().map(|x| x.as_i64().unwrap()).collect()),
+        json::JsonValue::Array(vec) => {
+            AoCResult::NumVec(vec.iter().map(|x| x.as_i64().unwrap()).collect())
+        }
         _ => panic!("Unsupported json val"),
     }
 }
@@ -62,9 +64,16 @@ fn test_hash_json() -> TestJsonHash {
         for test_info in tests {
             if let json::JsonValue::Array(ref test_info) = test_info {
                 if let json::JsonValue::Array(ref results) = test_info[3] {
-                    test_hash.entry(test_info[0].as_str().unwrap().to_string())
+                    test_hash
+                        .entry(test_info[0].as_str().unwrap().to_string())
                         .or_insert(Vec::new())
-                        .push((test_info[2].as_str().unwrap().to_string(), (json_to_aocresult(&results[0]), json_to_aocresult(&results[1]))));
+                        .push((
+                            test_info[2].as_str().unwrap().to_string(),
+                            (
+                                json_to_aocresult(&results[0]),
+                                json_to_aocresult(&results[1]),
+                            ),
+                        ));
                 }
             }
         }
@@ -72,21 +81,18 @@ fn test_hash_json() -> TestJsonHash {
     test_hash
 }
 
-
 #[rstest]
 #[case("d01", d01::p1p2 as AoCSolver)]
 #[case("d02", d02::p1p2 as AoCSolver)]
 #[case("d03", d03::p1p2 as AoCSolver)]
-fn day_test_json(
-    test_hash_json: &TestJsonHash,
-    #[case] day: &str,
-    #[case] test_func: AoCSolver,
-) {
+fn day_test_json(test_hash_json: &TestJsonHash, #[case] day: &str, #[case] test_func: AoCSolver) {
     for (input_file, expected_result) in test_hash_json.get(day).expect("Can't find day in hash") {
         let input_path = format!("{}{}", "../input/", input_file);
         println!("Testing {}", input_file);
         match expected_result {
-            (AoCResult::Number(n1), AoCResult::Number(n2)) => assert_eq!((*n1, *n2), test_func(&input_path)),
+            (AoCResult::Number(n1), AoCResult::Number(n2)) => {
+                assert_eq!((*n1, *n2), test_func(&input_path))
+            }
             _ => panic!("Unsupported result type"),
         }
     }

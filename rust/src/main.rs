a6357ae248b4e8f74682bd691d6acf49b75c0f51
Christopher Davidson <chdavids@cisco.com>
2022-12-07 01:25:42 +0000

    mess with rust testing

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,6 +9,13 @@ use rstest::rstest;
 
 type AoCSolver = fn(&str) -> (i32, i32);
 
+#[derive(Debug)]
+enum AoCResult {
+    Number(i32),
+    String(String),
+    NumVec(Vec<i64>),
+}
+
 const TEST_MAP: [(&str, AoCSolver); 3] = [
     ("d01", d01::p1p2 as AoCSolver),
     ("d02", d02::p1p2 as AoCSolver),
@@ -30,53 +37,57 @@ fn main() {
     }
 
     // Check the answers load
-    let _ = test_hash();
+    println!("THJ {:?}", test_hash_json());
 }
+
+fn json_to_aocresult(json_val: &json::JsonValue) -> AoCResult {
+    match json_val {
+        json::JsonValue::Short(a_string) => AoCResult::String(a_string.to_string()),
+        json::JsonValue::Number(_num) => AoCResult::Number(json_val.as_i32().unwrap()),
+        json::JsonValue::Array(vec) => AoCResult::NumVec(vec.iter().map(|x| x.as_i64().unwrap()).collect()),
+        _ => panic!("Unsupported json val"),
+    }
+}
+
+type TestJsonHash = HashMap<String, Vec<(String, (AoCResult, AoCResult))>>;
 
 #[rstest::fixture]
 #[once]
-fn test_hash() -> HashMap<String, (String, String)> {
-    println!("Loading test hash");
+fn test_hash_json() -> TestJsonHash {
+    println!("Loading test hash json");
     let mut test_hash = HashMap::new();
-    let answer_re =
-        regex::Regex::new(r"^(?P<part>d[^ ]+) (?P<input_file>[^ ]+) (?P<result>.*)").unwrap();
-    //let str_result_re = Regex::new(r"\(\"(?P<p1>.+)\", \"(?P<p2>.+)\"\)").unwrap();
-    for line in utils::read_lines("../answers").unwrap() {
-        let line = line.unwrap();
-        if line.starts_with("#") {
-            continue;
+    let json = std::fs::read_to_string("../answers.json").expect("Unable to read json answers");
+    let json = json::parse(&json).unwrap();
+    if let json::JsonValue::Array(ref tests) = json {
+        for test_info in tests {
+            if let json::JsonValue::Array(ref test_info) = test_info {
+                if let json::JsonValue::Array(ref results) = test_info[3] {
+                    test_hash.entry(test_info[0].as_str().unwrap().to_string())
+                        .or_insert(Vec::new())
+                        .push((test_info[2].as_str().unwrap().to_string(), (json_to_aocresult(&results[0]), json_to_aocresult(&results[1]))));
+                }
+            }
         }
-        let caps = answer_re.captures(&line).expect("Failed to parse line");
-        test_hash.insert(
-            // Just keep the first 3 chars, the rest is for python to know what
-            // method to call, but that is specified manually for rust
-            caps["part"][..3].to_string(),
-            (caps["input_file"].to_string(), caps["result"].to_string()),
-        );
     }
     test_hash
 }
+
 
 #[rstest]
 #[case("d01", d01::p1p2 as AoCSolver)]
 #[case("d02", d02::p1p2 as AoCSolver)]
 #[case("d03", d03::p1p2 as AoCSolver)]
-fn day_test(
-    test_hash: &HashMap<String, (String, String)>,
+fn day_test_json(
+    test_hash_json: &TestJsonHash,
     #[case] day: &str,
     #[case] test_func: AoCSolver,
 ) {
-    let (input_file, expected_result) = test_hash.get(day).expect("Can't find day in hash");
-    let int_result_re = regex::Regex::new(r"\((?P<p1>[0-9]+), (?P<p2>[0-9]+)\)").unwrap();
-    let retval_caps = int_result_re
-        .captures(&expected_result)
-        .expect("Failed to parse result");
-    let expected_result = (
-        retval_caps["p1"].parse::<i32>().unwrap(),
-        retval_caps["p2"].parse::<i32>().unwrap(),
-    );
-    assert_eq!(
-        expected_result,
-        test_func(&format!("{}{}", "../input/", input_file))
-    );
+    for (input_file, expected_result) in test_hash_json.get(day).expect("Can't find day in hash") {
+        let input_path = format!("{}{}", "../input/", input_file);
+        println!("Testing {}", input_file);
+        match expected_result {
+            (AoCResult::Number(n1), AoCResult::Number(n2)) => assert_eq!((*n1, *n2), test_func(&input_path)),
+            _ => panic!("Unsupported result type"),
+        }
+    }
 }

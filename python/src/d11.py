fbe0346c5fc9935164f2f726bee87dbdd798cf87
Christopher Davidson <chdavids@cisco.com>
2022-12-14 12:10:05 +0000

    half baked attempt to improve d11

--- python/src/d11.py
+++ python/src/d11.py
@@ -11,7 +11,7 @@ from functools import partial
 from collections import deque
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Callable
+from typing import Callable, NamedTuple
 
 import utils
 
@@ -60,6 +60,7 @@ def run_rounds(monkeys: list[Monkey], div_3: bool = False, rounds: int = 10_000)
     monkey_from_num: dict[int, Monkey] = {m.number: m for m in monkeys}
 
     common_mod = prod([m.test_div_by for m in monkeys])
+    print(f"{common_mod=}")
     for _ in range(rounds):
         for monkey in monkeys:
             while monkey.items:
@@ -83,12 +84,58 @@ def run_rounds(monkeys: list[Monkey], div_3: bool = False, rounds: int = 10_000)
     return inspect_counts[-2] * inspect_counts[-1]
 
 
+class ItemState(NamedTuple):
+    worry: int
+    monkey: int
+
+
+def per_items(monkeys: list[Monkey]) -> None:
+    monkey_from_num: dict[int, Monkey] = {m.number: m for m in monkeys}
+    common_mod = prod([m.test_div_by for m in monkeys])
+    worry_map: dict[ItemState, ItemState] = {}
+    for monkey in monkeys:
+        for worry in monkey.items:
+            train_id = ItemState(worry, monkey.number)
+            state = train_id
+            per_round_inspects = []
+            this_round_inspects = [0] * len(monkeys)
+            looped = False
+            in_cache = False
+            while True:
+                this_round_inspects[state.monkey] += 1
+                next_state = worry_map.get(state, None)
+                if next_state is None:
+                    this_monkey = monkey_from_num[state.monkey]
+                    next_worry = this_monkey.operation(state.worry) % common_mod
+                    next_monkey = this_monkey.if_true_monkey if next_worry % this_monkey.test_div_by == 0 else this_monkey.if_false_monkey
+                    next_state = ItemState(next_worry, next_monkey)
+                    worry_map[state] = next_state
+                else:
+                    in_cache = True
+
+                looped = looped or next_state == train_id
+                if next_state.monkey < state.monkey:
+                    # The next step will be on the next round
+                    per_round_inspects.append(this_round_inspects)
+                    if looped or in_cache:
+                        # End of round after looping so break
+                        break
+                    this_round_inspects = [0] * len(monkeys)
+                elif looped or in_cache:
+                    # We hit the cache but aren't at the end of a round
+                    print(f"{train_id} hit cycle ({looped} {in_cache}) after {len(per_round_inspects)} rounds but not at round end")
+
+                state = next_state
+            print(f"{train_id} {len(per_round_inspects)=} {len(worry_map)}")
+
+
 def p1p2(input_file: Path = real) -> tuple[int, int]:
     input_lines = input_file.read_text().splitlines()
     monkeys = [
         Monkey.from_lines(input_lines[x : x + 7]) for x in range(0, len(input_lines), 7)
     ]
     init_states = [m.items.copy() for m in monkeys]
+    per_items(monkeys)
     p1 = run_rounds(monkeys, div_3=True, rounds=20)
 
     # Reset monkeys for p2
@@ -103,5 +150,5 @@ def p1p2(input_file: Path = real) -> tuple[int, int]:
 
 
 if __name__ == "__main__":
-    print(p1p2(utils.example_input()))
+    #print(p1p2(utils.example_input()))
     print(p1p2(real))

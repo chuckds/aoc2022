b64306d9aa142074fa2b9b8c43be7a5a7f708fa1
Christopher Davidson <chdavids@cisco.com>
2022-12-19 23:59:52 +0000

    d16 p1

--- python/src/d16.py
+++ python/src/d16.py
@@ -21,33 +21,63 @@ class Valve:
     time_to_valve: dict[str, int] = field(default_factory=dict)
 
 
-def max_flow(at_valve: Valve, time_left: int, valves_to_open: set[str], release_rate: int, pressure_released: int, valves: dict[str, Valve]) -> int:
-    times = [(v_str, at_valve.time_to_valve[v_str] + 1)
-             for v_str in valves_to_open
-             if at_valve.time_to_valve[v_str] + 1 <= time_left]
-    if not times:
-        return pressure_released + release_rate * time_left
-    else:
-        max_flows = []
-        for valve_to_open_str, time_to_get_there in times:
-            valve_to_open = valves[valve_to_open_str]
-            next_valves_to_open = valves_to_open.copy()
-            next_valves_to_open.remove(valve_to_open_str)
-            max_flows.append(max_flow(valve_to_open, time_left - time_to_get_there, next_valves_to_open,
-                                      release_rate + valve_to_open.flow_rate, pressure_released + time_to_get_there * release_rate, valves))
-        return max(max_flows)
+def max_flow(at_valve: Valve, time_left: int, valves_to_open: set[str],
+             release_rate: int, valves: dict[str, Valve]) -> int:
+    max_flows = [release_rate * time_left]  # Do nothing flow
+
+    valve_open_times = [
+        (valves[v_str], at_valve.time_to_valve[v_str] + 1)
+        for v_str in valves_to_open
+        if at_valve.time_to_valve[v_str] + 1 <= time_left
+    ]
+    for valve_to_open, time_to_open in valve_open_times:
+        next_valves_to_open = valves_to_open.copy()
+        next_valves_to_open.remove(valve_to_open.name)
+        max_flows.append(time_to_open * release_rate +
+                         max_flow(valve_to_open,
+                                  time_left - time_to_open,
+                                  next_valves_to_open,
+                                  release_rate + valve_to_open.flow_rate,
+                                  valves))
+
+    return max(max_flows)
 
 
-def compute_times_from_valve(start_valve: Valve) -> None:
+def max_flow_two(at_valve: Valve, e_at_valve: Valve, time_left: int,
+                 valves_to_open: set[str],
+                 release_rate: int,
+                 valves: dict[str, Valve]) -> int:
+    max_flows = [release_rate * time_left]  # Do nothing flow
+
+    valve_open_times = [
+        (valves[v_str], at_valve.time_to_valve[v_str] + 1)
+        for v_str in valves_to_open
+        if at_valve.time_to_valve[v_str] + 1 <= time_left
+    ]
+    for valve_to_open, time_to_open in valve_open_times:
+        next_valves_to_open = valves_to_open.copy()
+        next_valves_to_open.remove(valve_to_open.name)
+        max_flows.append(time_to_open * release_rate +
+                         max_flow(valve_to_open,
+                                  time_left - time_to_open,
+                                  next_valves_to_open,
+                                  release_rate + valve_to_open.flow_rate,
+                                  valves))
+
+    return max(max_flows)
+
+
+def shortest_paths(start_valve: Valve) -> None:
     to_visit = deque([(start_valve, 0)])
     visited: dict[str, int] = {}
     while to_visit:
         visiting, path_so_far = to_visit.pop()
-        visited[visiting.name] = path_so_far
-        for valve in visiting.connected_valves:
-            next_path = path_so_far + 1
-            if valve.name not in visited:
-                to_visit.appendleft((valve, next_path))
+        if visiting.name not in visited:
+            visited[visiting.name] = path_so_far
+            for valve in visiting.connected_valves:
+                next_path = path_so_far + 1
+                if valve.name not in visited:
+                    to_visit.appendleft((valve, next_path))
     start_valve.time_to_valve = visited
 
 
@@ -62,9 +92,11 @@ def p1p2(input_file: Path = utils.real_input()) -> tuple[int, int]:
         v.connected_valves = [valves[valve_str] for valve_str in v.connected_valve_strs]
 
     for valve in valves.values():
-        compute_times_from_valve(valve)
+        shortest_paths(valve)
 
-    p1 = max_flow(valves["AA"], 30, set(v.name for v in valves.values() if v.flow_rate), 0, 0, valves)
+    valves_to_open = set(v.name for v in valves.values() if v.flow_rate > 0)
+    p1 = max_flow(valves["AA"], 30, valves_to_open, 0, valves)
+    #p2 = max_flow_two(valves["AA"], valves["AA"], 26, valves_to_open, 0, valves)
     return (p1, p2)
 
 

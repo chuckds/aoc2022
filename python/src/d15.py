4ea14d9413864359aa581438bc6c0fee3b4bfe20
Christopher Davidson <chdavids@cisco.com>
2022-12-19 00:49:02 +0000

    python d15

---
+++ python/src/d15.py
@@ -0,0 +1,102 @@
+"""
+Advent Of Code 2022 Day 15
+"""
+
+from __future__ import annotations
+
+
+import re
+from pathlib import Path
+from typing import NamedTuple
+
+import utils
+
+
+input_line_re = re.compile(r"[^=]*=(?P<sensx>-?[0-9]+)[^=]*=(?P<sensy>-?[0-9]+)[^=]*=(?P<beacx>-?[0-9]+)[^=]*=(?P<beacy>-?[0-9]+)")
+
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+    def taxicab_dist(self, other: Point) -> int:
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+
+def detection_slice_r(detection_size: int, from_centre: int, centre_x: int) -> range:
+    # from_centre   start x .  len x
+    # 0         centre_x - detection_size    detection_size * 2 + 1
+    # 1
+    return range(centre_x - detection_size + from_centre, centre_x + detection_size - from_centre + 1)
+
+
+def len_ranges(ranges: list[range]) -> int:
+    counted_upto = -1_000_000_000
+    res = 0
+    for r in sorted(ranges, key=lambda x: x.start):
+        res += max(counted_upto, r.stop) - max(counted_upto, r.start)
+        counted_upto = max(counted_upto, r.stop)
+    return res
+
+
+def p1(sensor_beacons: list[tuple[Point, Point, int]]) -> int:
+    target_row = 10 if len(sensor_beacons) < 15 else 2_000_000
+
+    range_block_on_target: list[range] = []
+    beacons_on_target_row: set[Point] = set()
+    for sensor, beacon, detection_size in sensor_beacons:
+        distance_from_target = abs(sensor.y - target_row)
+        if beacon.y == target_row:
+            beacons_on_target_row.add(beacon)
+        if detection_size >= distance_from_target:
+            range_block_on_target.append(detection_slice_r(detection_size, distance_from_target, sensor.x))
+
+    return len_ranges(range_block_on_target) - len(beacons_on_target_row)
+
+
+def p2(sensor_beacons: list[tuple[Point, Point, int]]) -> int:
+    dim_max = 20 if len(sensor_beacons) < 15 else 4_000_000
+
+    row_ranges: dict[int, list[range]] = {}
+    for sensor, beacon, detection_size in sensor_beacons:
+        if sensor.x + detection_size >= 0 and sensor.x - detection_size <= dim_max:
+            for row in range(max(0, sensor.y - detection_size), min(dim_max, sensor.y + detection_size) + 1):
+                distance_from_target = abs(sensor.y - row)
+                blocked_range = detection_slice_r(detection_size, distance_from_target, sensor.x)
+                row_ranges.setdefault(row, []).append(blocked_range)
+
+    x_val = -10
+    for row, blocked_ranges in row_ranges.items():
+        counted_upto = 0
+        for r in sorted(blocked_ranges, key=lambda x: x.start):
+            range_from = max(counted_upto, r.start)
+            if range_from > counted_upto:
+                # Break in the range
+                x_val = counted_upto
+                break
+            counted_upto = max(counted_upto, r.stop)
+            if counted_upto > dim_max:
+                break
+        if x_val >= 0:
+            return x_val * 4_000_000 + row
+
+    return 0
+
+
+def p1p2(input_file: Path = utils.real_input()) -> tuple[int, int]:
+    sensor_beacons: list[tuple[Point, Point, int]] = []
+    beacons: set[Point] = set()
+    for line in input_file.read_text().splitlines():
+        m = input_line_re.match(line)
+        if m:
+            sensor = Point(int(m.group("sensx")), int(m.group("sensy")))
+            beacon = Point(int(m.group("beacx")), int(m.group("beacy")))
+            beacons.add(beacon)
+            sensor_beacons.append((sensor, beacon, sensor.taxicab_dist(beacon)))
+
+    return (p1(sensor_beacons), p2(sensor_beacons))
+
+
+if __name__ == "__main__":
+    print(p1p2(utils.example_input()))
+    print(p1p2(utils.real_input()))
---
+++ rust/src/d15.rs
@@ -0,0 +1,13 @@
+use crate::utils::*;
+
+pub fn p1p2(input_file: &str) -> AoCSolver {
+    let mut p1: i64 = 0;
+    let mut p2: i64 = 0;
+
+    for line in read_lines(input_file).unwrap() {
+        let line = line.unwrap();
+    }
+
+    AoCSolver::BothParts(AoCResult::Number(p1),
+                         AoCResult::Number(p2))
+}
